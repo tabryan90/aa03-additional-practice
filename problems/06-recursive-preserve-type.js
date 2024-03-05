25a48cf8879e10041e86c594837a4392a53563f1
Alex Bryan <tabryan1990@gmail.com>
2024-03-04 21:58:40 -0500

    save

--- problems/06-recursive-preserve-type.js
+++ problems/06-recursive-preserve-type.js
@@ -25,10 +25,20 @@ console.log(preserveFunc2('boolean')); // prints [ true, false ]
 Note: the mocha tests do not test that your solution is implemented recursively.
 However, for bonus points try to solve it both with recursion and iteration.
 */
+debugger
 
 function recursivePreserveType(array) {
-    // Your code here 
+
 }
+
+const preserveFunc = recursivePreserveType([1, 'one', 2, 'two', 3, 'three']);
+console.log(preserveFunc('number')); // prints [1, 2, 3]
+console.log(preserveFunc('string')); // prints ['one', 'two', 'three']
+
+const preserveFunc2 = recursivePreserveType([2, undefined, 'world', { color: 'red' }, true, 3, [4, 5], 'hello', false]);
+console.log(preserveFunc2('number')); // prints [2, 3]
+console.log(preserveFunc2('object')); // prints [ { color: 'red' }, [4, 5] ]
+console.log(preserveFunc2('boolean')); // prints [ true, false ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
